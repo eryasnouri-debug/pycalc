f78ed083afb11492e0a501638555955361da787f
Eryas <eryasnouri@gmail.com>
2026-02-26 11:57:24 +0100

    Add files via upload

---
+++ pycalc.py
@@ -0,0 +1,14 @@
+
+def calcul(number1,signe,number2):
+    if signe == "+":
+        return number1 + number2
+    elif signe == "-":
+        return number2 - number1
+    elif signe == "/":
+        return number1 / number2
+    elif signe == "*":
+        return number1 * number2 
+    else:
+        print("nah")
+a = calcul(20, "+", 30)
+print(a)
